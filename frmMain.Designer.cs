daabcd1b5a469de12f14e910a844d497fb22af26
Florian Dahn <florian.dahn@fdsoftware.de>
2020-08-19 23:46:18 +0200

    Initial commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace PBPubkeyConverter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPublicKeyIn = new System.Windows.Forms.TextBox();
+            this.bnConvert = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtConverted = new System.Windows.Forms.TextBox();
+            this.llLink = new System.Windows.Forms.LinkLabel();
+            this.cmbTypes = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(589, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This easy tool let you \"convert\" a non xpub public key to any other key which is " +
+    "supported.";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(13, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(589, 62);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "There is no magic behind, cause you have to replace the first 4 bytes with the on" +
+    "es of the one you want to convert to. You just have to take about that Base64 en" +
+    "coding.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "The public key to convert:";
+            // 
+            // txtPublicKeyIn
+            // 
+            this.txtPublicKeyIn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPublicKeyIn.Location = new System.Drawing.Point(13, 146);
+            this.txtPublicKeyIn.Multiline = true;
+            this.txtPublicKeyIn.Name = "txtPublicKeyIn";
+            this.txtPublicKeyIn.Size = new System.Drawing.Size(589, 81);
+            this.txtPublicKeyIn.TabIndex = 2;
+            // 
+            // bnConvert
+            // 
+            this.bnConvert.Location = new System.Drawing.Point(12, 286);
+            this.bnConvert.Name = "bnConvert";
+            this.bnConvert.Size = new System.Drawing.Size(75, 23);
+            this.bnConvert.TabIndex = 3;
+            this.bnConvert.Text = "Convert";
+            this.bnConvert.UseVisualStyleBackColor = true;
+            this.bnConvert.Click += new System.EventHandler(this.bnConvert_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "The converted result:";
+            // 
+            // txtConverted
+            // 
+            this.txtConverted.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtConverted.Location = new System.Drawing.Point(12, 353);
+            this.txtConverted.Multiline = true;
+            this.txtConverted.Name = "txtConverted";
+            this.txtConverted.Size = new System.Drawing.Size(589, 81);
+            this.txtConverted.TabIndex = 2;
+            // 
+            // llLink
+            // 
+            this.llLink.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.llLink.AutoSize = true;
+            this.llLink.Location = new System.Drawing.Point(174, 533);
+            this.llLink.Name = "llLink";
+            this.llLink.Size = new System.Drawing.Size(295, 15);
+            this.llLink.TabIndex = 4;
+            this.llLink.TabStop = true;
+            this.llLink.Text = "powered by PREMIUM BLACK (https://premium.black)";
+            this.llLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llLink_LinkClicked);
+            // 
+            // cmbTypes
+            // 
+            this.cmbTypes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTypes.FormattingEnabled = true;
+            this.cmbTypes.Location = new System.Drawing.Point(85, 248);
+            this.cmbTypes.Name = "cmbTypes";
+            this.cmbTypes.Size = new System.Drawing.Size(161, 23);
+            this.cmbTypes.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 251);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Convert to:";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.Location = new System.Drawing.Point(12, 493);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(590, 24);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Huge thanks to NBitcoin (https://github.com/MetacoSA/NBitcoin)";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 557);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbTypes);
+            this.Controls.Add(this.llLink);
+            this.Controls.Add(this.bnConvert);
+            this.Controls.Add(this.txtConverted);
+            this.Controls.Add(this.txtPublicKeyIn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "PREMIUM BLACK Pubkey Converter";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPublicKeyIn;
+        private System.Windows.Forms.Button bnConvert;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtConverted;
+        private System.Windows.Forms.LinkLabel llLink;
+        private System.Windows.Forms.ComboBox cmbTypes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBPubkeyConverter
+{
+    public partial class frmMain : Form
+    {
+
+        public Dictionary<String, String> PubKeyPrefixes = new Dictionary<String, String>()
+        {
+            ["xpub"] = "0488b21e",
+            ["ypub"] = "049d7cb2",
+            ["Ypub"] = "0295b43f",
+            ["zpub"] = "04b24746",
+            ["Zpub"] = "02aa7ed3",
+            ["tpub"] = "043587cf",
+            ["upub"] = "044a5262",
+            ["Upub"] = "024289ef",
+            ["vpub"] = "045f1cf6",
+            ["Vpub"] = "02575483",
+            ["drkp"] = "02fe52cc",
+            ["drkv"] = "02fe52f8",
+            ["DRKP"] = "3a805837",
+            ["DRKV"] = "3a8061a0",
+            ["tpub"] = "043587cf",
+            ["tprv"] = "04358394",
+            ["Ltub"] = "019da462",
+        };
+
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// xpub = 0488b21e
+        /// 
+        /// </summary>
+        /// <param name="xpubkey"></param>
+        /// <param name="newFormat"></param>
+        /// <returns></returns>
+        public string ParseNewxPubFormats(string xpubkey, string newFormat = "xpub")
+        {
+            try
+            {
+                var encoder = new NBitcoin.DataEncoders.Base58CheckEncoder();
+                var decoded = encoder.DecodeData(xpubkey);
+
+                var decoded_2 = decoded.Skip(4).ToArray();
+
+                var decoded_3 = StringToByteArray(PubKeyPrefixes[newFormat]).Concat(decoded_2).ToArray();
+
+                var encoded = encoder.EncodeData(decoded_3);
+
+                return encoded;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        private void llLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenUrl("https://premium.black");
+        }
+
+
+
+        static void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    var p = Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+
+                    Thread.Sleep(1000);
+
+                    p.Close();
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void bnConvert_Click(object sender, EventArgs e)
+        {
+            if(!PubKeyPrefixes.ContainsKey(cmbTypes.SelectedItem.ToString()))
+            {
+                return;
+            }
+
+
+            var s = ParseNewxPubFormats(txtPublicKeyIn.Text, cmbTypes.SelectedItem.ToString());
+
+            txtConverted.Text = s;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("DISCLAIMER: Please use this tool on your own RISK.\r\nIt can help you, to get your Public Key (xPub) in a different format for PREMIUM BLACK or other services. This tool will be provided \"as-is\" without any warranty about its functionality. But at any time prove the given public key and compare with the one you'll receive from your wallet.\r\nWhen you press on OK you agree with that.", "DISCLAIMER", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) != DialogResult.OK)
+            {
+                this.Close();
+                return;
+            }
+
+            loadTypes();
+
+            cmbTypes.SelectedItem = "xpub";
+        }
+
+        private void loadTypes()
+        {
+            
+
+            foreach(var k in PubKeyPrefixes.OrderBy(a => a.Key))
+            {
+                cmbTypes.Items.Add(k.Key);
+
+
+            }
+        }
+    }
+}
