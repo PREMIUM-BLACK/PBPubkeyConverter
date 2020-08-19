bf9422a4b01ce39df82eccc253db6ead757e34e5
Florian Dahn <florian.dahn@fdsoftware.de>
2020-08-19 23:49:01 +0200

    adding net472 framework

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace PBPubkeyConverter
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
